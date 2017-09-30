0bbe4ce8254cc3be97072b8df7c865271a50ba12
Mogens Lund <salza@salza.dk>
2017-09-30 20:23:01 +0200

    Now using Clojure 1.9.0-beta1

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0-beta1"]
                  [mogenslund/liquid "0.8.0"]]
   :resource-paths ["resources"]
-  :target-path "/tmp/additives/target/%s/"
+  :target-path "/tmp/liq/target/%s/"
   :clean-targets ^{:protect false} [:target-path]
   :profiles {:uberjar {:aot :all}})
