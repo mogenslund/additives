ee9b153672fd5356267ca30b4b894a3df318ab10
Mogens Brødsgaard Lund <salza@salza.dk>
2017-12-14 21:30:17 +0100

    Making the repo more slim to fit new extensions strategy.

--- test/dk/salza/additives/blob_test.clj
+++
@@ -1,7 +0,0 @@
-(ns dk.salza.additives.blob-test
-  (:require [clojure.test :refer :all]
-            [dk.salza.additives.blob :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
