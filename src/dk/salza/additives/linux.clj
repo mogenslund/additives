154c80e52b798e092e18f3e33ea7748238051893
Mogens Brødsgaard Lund <salza@salza.dk>
2018-02-22 16:49:49 +0100

    Explicit grep and git handles

--- src/dk/salza/additives/linux.clj
+++ src/dk/salza/additives/linux.clj
@@ -5,18 +5,29 @@
 
 (defn curl
   [& args]
-  (let [inputmap (if (map? (first args)) (first args) (apply hash-map args))
+  (let [sp " " ; ""
+        inputmap (if (map? (first args)) (first args) (apply hash-map args))
         {:keys [:method :url :data :headers :other :dry]} inputmap
         parameters (concat
                      (list "curl")
                      (when method (list "-X" (str/upper-case (name method))))
                      (when data (list "-d" (str/join "&" (for [[k v] data] (str (name k) "=" (str/replace v #"&" "%26"))))))
-                     (when headers (apply concat (for [[k v] headers] (list "-H" (str k ": " v) ))))
+                     (when headers (apply concat (for [[k v] headers] (list "-H" (str k ":" sp v) ))))
                      other
                      (list url))]
     (if dry
-      (str/join " " parameters)
+      (str/join sp parameters)
       (apply sh/cmd parameters))))
+
+(defn git
+  [& args]
+  (let [gitdir (first (reverse args))
+        gitargs (reverse (rest (reverse args)))]
+    (apply sh/cmdseq (concat (list "git" "--work-tree" gitdir "--git-dir" (str gitdir "/.git")) gitargs))))
+
+(defn grep
+  [& args]
+  (apply sh/cmdseq (concat (list "grep") args)))
 
 (defn bash
   ([line]
