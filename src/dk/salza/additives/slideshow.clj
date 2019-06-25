2df5927e530a012413a3f58c2faf177d9eb4f0ab
Mogens Brødsgaard Lund <salza@salza.dk>
2019-06-25 23:08:44 +0200

    More keybinding options in slideshow

--- src/dk/salza/additives/slideshow.clj
+++ src/dk/salza/additives/slideshow.clj
@@ -1,5 +1,7 @@
 (ns dk.salza.additives.slideshow
   (:require [clojure.string :as str]
+            [dk.salza.liq.keymappings.normal]
+            [dk.salza.liq.keymappings.insert]
             [dk.salza.liq.editor :as editor]))
 
 (def slides (atom ["This is page 1" "This is page 2" "This is page 3"]))
@@ -32,18 +34,37 @@
     (update-display)))
 
 (def keymap
-  {:cursor-color :off
+  {:id "dk.salza.additives.slideshow.page"
+   :cursor-color :off
+   "\t" #(editor/set-keymap "dk.salza.additives.slideshow.normal")
+   "i" #(editor/set-keymap "dk.salza.additives.slideshow.insert")
+   "o" #(do (editor/insert-line) (editor/set-keymap "dk.salza.additives.slideshow.insert"))
    "C-w" editor/kill-buffer
    "C-g" editor/previous-real-buffer
    "esc" editor/previous-real-buffer
    " " next-slide
+   "l" next-slide
+   "j" #(editor/set-keymap "dk.salza.additives.slideshow.normal")
    "right" next-slide
    "left" previous-slide
+   "h" previous-slide
+   "k" #(editor/set-keymap "dk.salza.additives.slideshow.normal")
   })
 
 (defn run
   [& keyws]
   (reset! slides (vec (map blob keyws)))
   (editor/new-buffer "-slideshow-")
-  (editor/set-keymap keymap)
+  (editor/add-keymap (assoc dk.salza.liq.keymappings.normal/keymapping
+                       :id "dk.salza.additives.slideshow.normal"
+                       "i" #(editor/set-keymap "dk.salza.additives.slideshow.insert")
+                       "o" #(do (editor/insert-line) (editor/set-keymap "dk.salza.additives.slideshow.insert"))
+                       "esc" #(editor/set-keymap "dk.salza.additives.slideshow.page")
+                       "\t" #(editor/set-keymap "dk.salza.additives.slideshow.page")))
+  (editor/add-keymap (assoc dk.salza.liq.keymappings.insert/keymapping
+                       :id "dk.salza.additives.slideshow.insert"
+                       "esc" #(editor/set-keymap "dk.salza.additives.slideshow.normal")
+                       "\t" #(editor/set-keymap "dk.salza.additives.slideshow.normal")))
+  (editor/add-keymap keymap)
+  (editor/set-keymap "dk.salza.additives.slideshow.page")
   (update-display))
